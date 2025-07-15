3fc6fbe8cc94a3295ad9fdf33fb78223f109d418
Sharif Ahmed <sharif.ahmed+ottoint@ottoint.com>
2025-07-15 15:03:03 +0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello, I'm Sharif Ahmed!</h1>
+      <p>Welcome to my demo frontend application built with React.</p>
+      <p>This page was created as part of an exam project.</p>
+    </div>
+  );
+}
+
+export default App;
